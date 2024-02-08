278e398e995aeccfe47a220cb056f818be0827a5
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-02-08 17:02:52 +0300

    08.02 - 2

--- src/components/selectKabz/SelectKabz.tsx
+++ src/components/selectKabz/SelectKabz.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import S from './SelectKabz.module.css'
 
 export type ItemType = {
@@ -14,17 +14,31 @@ type SelectPropsType = {
 }
 
 export const SelectKabz = (props: SelectPropsType) => {
+    const [active, setActive] = useState<boolean>(false)
+
+    const toggleItems = () => {
+        setActive(!active)
+    }
+    const itemClicked = () => {
+
+    }
     const selectedItem = props.items.find(i => i.value === props.value)
 
     return (
-        <div className={S.select}>
-            <select>
-                <option value='1'>Moscow</option>
-                <option value='2'>Lima</option>
-                <option value='3'>London</option>
-            </select>
-            <h3>{selectedItem && selectedItem.title}</h3>
-            {props.items.map(i => <div>{i.title}</div>)}
+        <div className={`${S.select}`}>
+            <h3 className={S.main} onClick={toggleItems}>{selectedItem && selectedItem.title}</h3>
+            {active && <div className={S.items}>
+                {props.items.map(i => {
+                    return (
+                        <div key={i.value}
+                             onClick={itemClicked}
+                        >
+                            {i.title}
+                        </div>
+                    )
+                })}
+            </div>
+            }
         </div>
     )
 }
