24a551385cdb30ceb53c8ae1e98801e8738ebd53
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-03-03 11:53:36 +0300

    03.03 - useClear - 2

--- src/components/stories/useEffect.stories.tsx
+++ src/components/stories/useEffect.stories.tsx
@@ -56,3 +56,52 @@ export const TimeOutExample = () => {
         </>
     )
 }
+
+
+export const ResetUseEffectExample = () => {
+
+    const [counter, setCounter] = useState(1)
+
+
+    useEffect(() => {
+        console.log('UseEffect render: ' + counter)
+
+
+        return () => {
+            console.log('RESET EFFECT')
+        }
+    }, [counter])
+
+
+    return (
+        <>
+            <button onClick={() => setCounter(counter + 1)}>+</button>
+            <h1>Hello, counter: {counter}</h1>
+        </>
+    )
+}
+
+export const KeysTrackerExample = () => {
+
+    const [text, setText] = useState('')
+
+    const handlerEvents = (e: KeyboardEvent) => {
+        setText(text + e.key)
+        console.log(e.key)
+    }
+
+    useEffect(() => {
+        window.document.addEventListener('keypress', handlerEvents)
+
+        return () => {
+            window.document.removeEventListener('keypress', handlerEvents)
+        }
+    }, [text])
+
+
+    return (
+        <>
+            <h1>Hello, text: {text}</h1>
+        </>
+    )
+}
